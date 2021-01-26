979f3f0f2721f3e06a3100bf03fa8511570fea24
martinmazas <mazasmartin@gmail.com>
2021-01-26 20:20:21 +0200

    starting to work on task 1.3

--- main.py
+++ main.py
@@ -47,9 +47,70 @@ def main():
     # Task 1.3
     # 3. Use a filled contour plot to show the decision distribution of your model (limit your plot axes to\n",
     #     "the actual data boundaries +-1).
+    def bayes_plot(df, model="gnb", spread=30):
+        df.dropna()
+        colors = 'seismic'
+        col1 = df.columns[0]
+        col2 = df.columns[1]
+        target = df.columns[2]
+        sns.scatterplot(data=df, x=col1, y=col2, hue=target)
+        plt.show()
+        y = df[target]  # Target variable
+        X = df.drop(target, axis=1)
 
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,
+                                                            random_state=1)  # 80% training and 20% test
+
+        clf = GaussianNB()
+        if (model != "gnb"):
+            clf = DecisionTreeClassifier(max_depth=model)
+        clf = clf.fit(X_train, y_train)
+
+        # Train Classifer
+
+        prob = len(clf.classes_) == 2
+
+        # Predict the response for test dataset
+
+        y_pred = clf.predict(X_test)
+        print(metrics.classification_report(y_test, y_pred))
+
+        hueorder = clf.classes_
+
+        def numify(val):
+            return np.where(clf.classes_ == val)[0]
+
+        Y = y.apply(numify)
+        x_min, x_max = X.loc[:, col1].min() - 1, X.loc[:, col1].max() + 1
+        y_min, y_max = X.loc[:, col2].min() - 1, X.loc[:, col2].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.2),
+                             np.arange(y_min, y_max, 0.2))
+
+        Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])
+        if prob:
+
+            Z = Z[:, 1] - Z[:, 0]
+        else:
+            colors = "Set1"
+            Z = np.argmax(Z, axis=1)
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.contourf(xx, yy, Z, cmap=colors, alpha=0.5)
+        plt.colorbar()
+        if not prob:
+            plt.clim(0, len(clf.classes_) + 3)
+        sns.scatterplot(data=df[::spread], x=col1, y=col2, hue=target, hue_order=hueorder, palette=colors)
+        fig = plt.gcf()
+        fig.set_size_inches(12, 8)
+        plt.show()
+
+    input_values = pd.concat([penguinsData.bill_depth_mm, penguinsData.bill_length_mm], axis=1)
+    target_values = penguinsData.species
+    bayes_plot(pd.concat([input_values, target_values], axis=1), spread=1)
 
     # print(penguinsData)
+
 
 if __name__ == '__main__':
     main()
